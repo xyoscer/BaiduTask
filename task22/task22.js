6ce3b9ff4ae056d2a647f31b550438478f03fce9
郗银 <1498546245@qq.com>
2016-07-26 19:25:43 +0800

    使用事件代理，将代码简化

--- task22/task22.js
+++ task22/task22.js
@@ -10,27 +10,35 @@ function addEventHander(ele,event,handle){
 }
 var treeList = [];
 var root =  document.getElementById('root');
-var child1 = document.getElementsByClassName('child1');
 var btn = document.getElementsByTagName('button');
 var timer = null;
-addEventHander(btn[0],"click",dealNode);
-addEventHander(btn[1],"click",dealNode1);
-addEventHander(btn[2],"click",dealNode2);
-function dealNode(){
-	reset();
-	preOrder(root);
+var deal = document.getElementById('deal');
+
+addEventHander(deal,"click",treeDeal);
+
+/**
+   树的遍历的可视化分为三步：
+   第一步初始化树
+   第二步进行遍历树，将遍历的结果存入数组；
+   第三步对数组中的树按照顺序以某种动画某个时间显示出来
+*/
+function treeDeal(event){
+   var event = window.event||event;
+   var target = event.target||event.srcElement;
+   reset();
+   switch(target.id){
+   	 case "pre":preOrder(root);
+   	 break;
+   	 case "in":inOrder(root);
+   	 break;
+   	 case "post":postOrder(root);
+   	 break;
+   	 default:
+   	 break;
+   }
     changeStyle();
 }
-function dealNode1(){
-	reset();
-	inOrder(root);	
-    changeStyle();
-}
-function dealNode2(){
-    reset();
-	postOrder(root);		
-    changeStyle();
-}
+//先序遍历
 function preOrder(node){
    if (!(node == null)) {
 		treeList.push(node);		
@@ -38,14 +46,17 @@ function preOrder(node){
 		preOrder(node.lastElementChild);
 }
 }
+//中序遍历
 function inOrder(node){
     if (!(node == null)) {
 		
 		inOrder(node.firstElementChild);
 			treeList.push(node);	
 		inOrder(node.lastElementChild);
+
 } 
 }
+//后序遍历
 function postOrder(node){
     if (!(node == null)) {		
 		postOrder(node.firstElementChild);				
@@ -53,11 +64,11 @@ function postOrder(node){
 		treeList.push(node);
 } 
 }
-
+//每个1s,动画形式显示一次遍历的结果
 function changeStyle() {
 	var i = 0; 	
 	treeList[i].className += " animate";	
-	timer = setInterval(function (argument) {
+	timer = setInterval(function(){
 		i++;
 		if (i < treeList.length) {	
 
@@ -72,12 +83,13 @@ function changeStyle() {
 		}
 	},1000)
 }
+//初始化树
 function reset() {
-
+  
 	clearInterval(timer);
-	var divEles = document.getElementsByTagName("div");
-	for(var i=0; i<divEles.length;i++){
-		divEles[i].className = divEles[i].className.replace(/normal|animate/,"");
+	treeList.forEach(function(ele){
+         ele.className = ele.className.replace(/normal|animate/,"");
+	});
+	 treeList = [];
 }
 	
-}
